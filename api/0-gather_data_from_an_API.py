326f50587c6aa8ee91c6e4cb1ab679c26e348876
Faqu22 <facundoalvarez415@gmail.com>
2023-08-22 15:49:15 -0300

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,26 +5,25 @@
 """
 
 
-if __name__ == "__main__":
-    import json
-    import sys
-    from urllib import request
+import json
+import sys
+from urllib import request
 
-    empId = sys.argv[1]
+empId = sys.argv[1]
 
-    url = request.urlopen(
-        "https://jsonplaceholder.typicode.com/users/{}".format(empId))
+url = request.urlopen(
+    "https://jsonplaceholder.typicode.com/users/{}".format(empId))
 
-    info = json.loads(url.read().decode("utf-8"))
+info = json.loads(url.read().decode("utf-8"))
 
-    url_task = request.urlopen(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(empId))
-    tasks = json.loads(url_task.read().decode("utf-8"))
+url_task = request.urlopen(
+    "https://jsonplaceholder.typicode.com/todos?userId={}".format(empId))
+tasks = json.loads(url_task.read().decode("utf-8"))
 
-    completed = [task for task in tasks if task["completed"]]
+completed = [task for task in tasks if task["completed"]]
 
-    print("Employee {} is done with tasks ({}/{}):".
-          format(info["name"], len(completed), len(tasks)))
-    title = [task["title"] for task in completed]
-    for text in title:
-        print("\t {}".format(text))
+print("Employee {} is done with tasks ({}/{}):".
+        format(info["name"], len(completed), len(tasks)))
+title = [task["title"] for task in completed]
+for text in title:
+    print("\t {}".format(text))
